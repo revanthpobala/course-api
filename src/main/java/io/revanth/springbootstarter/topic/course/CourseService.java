d757868f6e6a67558a4b0619fb36d6fb5e0afaef
revanthpobala <revanth.linux@gmail.com>
2017-04-03 20:58:39 -0700

    Added new entity class called courses. Topics -> Courses

---
+++ src/main/java/io/revanth/springbootstarter/topic/course/CourseService.java
@@ -0,0 +1,37 @@
+package io.revanth.springbootstarter.topic.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseRepository courseRepository;
+
+	public List<Course> getAllCourses(String id) {
+		List<Course> result = new ArrayList<>();
+		courseRepository.findByTopicId(id).forEach(result::add);
+		return result;
+	}
+
+	public Course getCourse(String id) {
+		return courseRepository.findOne(id);
+	}
+
+	public void addCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.delete(id);
+	}
+
+}
